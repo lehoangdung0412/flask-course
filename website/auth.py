9d7d3fffecc6209102ac44ae327cfb5407e539f3
dunglh <dunglh@guide.inc>
2023-02-19 23:35:56 +0700

    create sign_up page and format code

--- website/auth.py
+++ website/auth.py
@@ -2,13 +2,16 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login')
 def login():
     return render_template("login.html", text="Testing", user="Vincent", boolean=True)
 
+
 @auth.route('/logout')
 def logout():
     return '<p>Logout</p>'
+
 
 @auth.route('/sign-up')
 def sign_up():
